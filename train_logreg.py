22e4c69bcc8b65ec0e680186de72cc89775c9832
aabinckes <156472645+aabinckes@users.noreply.github.com>
2026-03-03 20:23:29 +0000

    all the arguments that are going to be passed into ArgumentParser()

--- train_logreg.py
+++ train_logreg.py
@@ -24,4 +24,28 @@ def evaluate(y_true, y_pred):
         "f1": float(f1),
         "confusion_matrix": cm.tolist(),
     }
-    
+
+def main() -> None:
+    p = argparse.ArgumentParser()
+
+    #data/split
+    p.add_argument("--cache_dir", type=str, default=None)
+    p.add_argument("--val_size", type=float, default=0.2)
+    p.add_argument("--seed", type=int, default=42)
+    p.add_argument("--config_name", type=str, default=None)
+    p.add_argument("--no_clean", action="store_true")
+
+    #TF-IDF
+    p.add_argument("--max_features", type=int, default=50000)
+    p.add_argument("--ngram_min", type=int, default=1)
+    p.add_argument("--ngram_max", type=int, default=2)
+    p.add_argument("--min_df", type=int, default=2)
+    p.add_argument("--max_df", type=float, default=0.95)
+
+    #logistic Regression
+    p.add_argument("--C", type=float, default=1.0)
+    p.add_argument("--class_weight", type=str, default=None, choices=[None, "balanced"])
+    p.add_argument("--max_iter", type=int, default=2000)
+
+    #output
+    p.add_argument("--out_dir", type=str, default="results/logreg_baseline")
